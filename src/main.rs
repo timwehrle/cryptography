2f264147f7ddd99280ed48ac4d3a3fea5f16c604
timwehrle <wetigg@gmail.com>
2025-01-25 00:48:15 +0100

    Update testing of ciphers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use caesar_cipher::caesar_cipher;
+pub mod ciphers;
 
-pub mod caesar_cipher;
+use ciphers::caesar_cipher::caesar_cipher;
+use ciphers::vigenere_cipher::vigenere_cipher;
+
+fn test_cipher(
+    name: &str,
+    encrypt_fn: impl Fn(&str) -> String,
+    decrypt_fn: impl Fn(&str) -> String,
+) {
+    let message = "This is a test string!";
+    let encrypted = encrypt_fn(message);
+    println!("Encrypted {}: {}", name, encrypted);
+
+    let decrypted = decrypt_fn(&encrypted);
+    println!("Decrypted {}: {}", name, decrypted);
+}
 
 fn main() {
-    let message = "This is a test string!";
-
+    // Caesar Cipher Test
     let shift = 3;
+    test_cipher(
+        "Caesar Cipher",
+        |msg| caesar_cipher(msg, shift, true),
+        |msg| caesar_cipher(msg, shift, false),
+    );
 
-    let encrypted = caesar_cipher(message, shift, true);
-    println!("Encrypted: {}", encrypted);
-
-    let decrypted = caesar_cipher(&encrypted, shift, false);
-    println!("Decrypted: {}", decrypted);
+    // Vigenere Cipher Test
+    let key = "hello";
+    test_cipher(
+        "Vigenere Cipher",
+        |msg| vigenere_cipher(msg, key, true),
+        |msg| vigenere_cipher(msg, key, false),
+    );
 }
